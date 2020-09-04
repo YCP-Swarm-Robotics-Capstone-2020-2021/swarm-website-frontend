ac5f6bf5d3a8ba0b5c08a3582ccde77cd0188992
Walrussuit101 <tjefferson@ycp.edu>
2020-09-03 23:49:46 -0400

    chooses random bg image, added image prop for alt attribute

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,27 @@ import {TextField} from "./components/TextField";
 import {Image} from "./components/Image";
 
 //images
-const logo = require('./images/swarmLogoIcon.png')
+const logo = require('./images/swarmLogoIcon.png');
+let bgNum = Math.floor(Math.random() * 5) + 1
+
+const sectionStyle = {
+    backgroundImage: "url(" + require('./images/loginbgs/bg'+bgNum+'.jpg') + ")",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    minHeight: "100vh",
+    overflow: "hidden"
+};
+
 
 const App: React.FC = () => {
     return(
-        <div id='loginBox'>
-            <Image id='logo' src={String(logo)}/>
-            <TextField placeholder='Username'/>
-            <TextField placeholder='Password'/>
-        </div>
+        <section style={sectionStyle}>
+            <div id='loginBox'>
+                <Image id='logo' src={String(logo)} alt='Swarm Robotics Logo'/>
+                <TextField placeholder='Username'/>
+                <TextField placeholder='Password'/>
+            </div>
+        </section>
     );
 };
 
--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 //props for component to use
 interface Props{
     src: string;
+    alt: string;
     id: string;
 }
 
-export const Image: React.FC<Props> = ({src, id}) => {
+export const Image: React.FC<Props> = ({src, id, alt}) => {
     return(
         <div>
-            <img id ={id} src={src}></img>
+            <img id ={id} src={src} alt={alt}></img>
         </div>
     );
 };
