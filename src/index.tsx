174b5536f3921aaee7877acb80a4ade6f34a464d
Thomas <rithonn21@gmail.com>
2020-09-06 19:05:40 -0400

    Added react router ts support with a basic url change link

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LogIn from './components/LogIn/LogIn';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <LogIn />
+      <BrowserRouter>
+          <LogIn />
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
