a08c8077ca2d15313f7591292922f80f98923ec0
Walrussuit101 <tjefferson@ycp.edu>
2020-09-08 17:48:22 -0400

    added div for possible activity graph

--- src/components/Wiki/WikiLanding.tsx
+++ src/components/Wiki/WikiLanding.tsx
@@ -5,14 +5,19 @@ import './WikiLanding.css';
 class WikiLanding extends React.Component{
     render(){
         return(
-            <div id="statDiv">
-                <h1>This wiki has</h1>
-                <ul id="statList">
-                    <li>8 entries</li>
-                    <li>3 contributors</li>
-                    <li>26 comments</li>
-                    <li>12 images</li>
-                </ul>
+            <div>
+                <div id="statDiv">
+                    <h1>This wiki has</h1>
+                    <ul id="statList">
+                        <li>8 entries</li>
+                        <li>3 contributors</li>
+                        <li>26 comments</li>
+                        <li>12 images</li>
+                    </ul>
+                </div>
+                <div id="graphDiv">
+                    <h4>Graph of activity over time?</h4>
+                </div>
             </div>
         );
     }
