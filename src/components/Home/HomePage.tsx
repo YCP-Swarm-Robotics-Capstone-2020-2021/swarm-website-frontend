3aa4425b3e7ce3a48a6376a129c6c789bac0ec7b
Thomas McAdams <rithonn21@gmail.com>
2020-09-09 11:16:08 -0400

    Homepage setup

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -13,18 +13,15 @@ const styleSheet = styles();
 class HomePage extends React.Component{
 
     componentDidMount() {
-
+        //set the global body to the settings required
         document.getElementsByTagName("BODY")[0].classList.add('loginBody');
-
     }
 
     render(){
         return(
-
             <section style={background}>
                 <div> ADD FONT HERE</div>
             </section>
-
         )
     }
 }
