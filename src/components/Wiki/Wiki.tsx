90ca4d90ed19213648ee3e7050761b88557d8c40
Walrussuit101 <tjefferson@ycp.edu>
2020-09-14 15:07:09 -0400

    add navbar to wiki page

--- src/components/Wiki/Wiki.tsx
+++ src/components/Wiki/Wiki.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import './Wiki.css';
 
+import MainNavbar from "../../utils/MainNavbar";
 import EntryMenu from "./EntryMenu";
 import WikiLanding from "./WikiLanding";
 
 /*use states to switch between WikiLanding/WikiContent components in the rightPane div*/
+
+//get navbar logo
+const logo = require('../../images/swarmLogoIcon.png');
 
 class Wiki extends React.Component{
     componentDidMount() {
@@ -15,12 +19,16 @@ class Wiki extends React.Component{
 
     render(){
         return(
-          <div id='content'>
-              <EntryMenu />
-              <div id='rightPane'>
-                  <WikiLanding />
-              </div>
-          </div>
+            <div>
+                <MainNavbar logo={logo} />
+                <div id='content'>
+                  
+                  <EntryMenu />
+                  <div id='rightPane'>
+                      <WikiLanding />
+                  </div>
+                </div>
+            </div>
         );
     }
 }
