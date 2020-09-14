6548980f2ea665167aa4fa9362ceab2f332386c3
Thomas McAdams <rithonn21@gmail.com>
2020-09-14 18:08:55 -0400

    Added bycrypt and began reworking loginpage with bootstrap

--- src/components/LogIn/LogIn.tsx
+++ src/components/LogIn/LogIn.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
-
-import Image from "../../utils/Image";
-import TextField from '../../utils/TextField';
-import Button from '../../utils/Button';
 import backgroundImageStyling from '../../styles/backgroundImageStyling'
-
+import {Button, Form} from "react-bootstrap";
 import './LogIn.css';
 import {Link} from "react-router-dom";
+
 
 //require any images
 const logo = require('../../images/swarmLogoIcon.png');
 const background = backgroundImageStyling();
 
 class LogIn extends React.Component {
+    constructor(props: any) {
+        super(props);
+    }
     componentDidMount() {
-        // @ts-ignore, object could possibly be null
-        document.getElementsByTagName("BODY")[0].classList.add('loginBody');
-
         setTimeout(function (){
             // @ts-ignore, object could possibly be null
             document.getElementById('loginBox').classList.add('fade-in');
@@ -26,15 +23,25 @@ class LogIn extends React.Component {
     render() {
         return(
             <section style={background}>
-                <div id='loginBox'>
-                    <form>
-                        <Image id='logo' src={String(logo)} alt='Swarm Robotics Logo'/>
-                        <TextField id={'username'} class={'loginText'} name={'username'} type={'text'} placeholder='Username'/>
-                        <TextField id={'password'} class={'loginText'} name={'password'} type={'password'} placeholder='Password'/>
-                        <Link to="/home">
-                            <Button class={''} id={'loginButton'} type={"button"} text={"Log In"}/>
-                        </Link>
-                    </form>
+                <div id="loginBox">
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Text className={'text-center loginText'}>Email address</Form.Text>
+                            <Form.Control className={'loginTextInput text-center'} type="email" placeholder="Enter email" />
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Text className={'loginText text-center'}>Password</Form.Text>
+                            <Form.Control className={'loginTextInput text-center'} type="password" placeholder="Password" />
+                        </Form.Group>
+
+                        <Button id={'loginButton'} variant="primary" type="submit">
+                            Submit
+                        </Button>
+                        <Form.Text className={"text-muted loginText text-center"}>
+                           Forgot Password?
+                        </Form.Text>
+                    </Form>
                 </div>
             </section>
         );
