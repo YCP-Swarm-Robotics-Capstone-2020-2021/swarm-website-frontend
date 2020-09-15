407699aa15ad34406408ce98cd56ca04c3f0a5c3
Walrussuit101 <tjefferson@ycp.edu>
2020-09-14 22:47:23 -0400

    add entry sidebar

--- src/components/Wiki/Entry.tsx
+++ src/components/Wiki/Entry.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Tab, Tabs} from "react-bootstrap";
+import {Tab, Tabs, Card} from "react-bootstrap";
 
 import './Entry.css';
 
@@ -8,7 +8,35 @@ class Entry extends React.Component{
         return(
             <Tabs id="tabs" defaultActiveKey="details" variant="pills" bg="dark">
                 <Tab eventKey="details" title="Details">
-                    <p>Details tab</p>
+                    <Card id="sideBarCard">
+                        <Card.Body>
+                            <Card.Title>Milestone 1</Card.Title>
+                            <Card.Text>Test Text</Card.Text>
+                            <Card.Text>Test Text</Card.Text>
+                            <Card.Text>Test Text</Card.Text>
+                            <Card.Text>Test Text</Card.Text>
+                            <Card.Text>Test Text</Card.Text>
+                            <Card.Text>Test Text</Card.Text>
+                            <Card.Text>Test Text</Card.Text>
+                            <Card.Text>Test Text</Card.Text>
+                            <Card.Text>Test Text</Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card id="detailsCard" bg="dark" text="white">
+                        <h2>Milestone 1</h2>
+                        <Card.Body>
+                            <Card.Title>A heading</Card.Title>
+                                <Card.Text>The text that goes with the above heading. Crazy stuff
+                                    Crazy stuffCrazy stuffCrazy stuffCrazy stuffCrazy stuffCrazy stuffCrazy stuffCrazy stuff
+                                    Crazy stuffCrazy stuffCrazy stuffCrazy stuffCrazy stuffCrazy stuffCrazy stuffCrazy stuff
+                                    Crazy stuffCrazy stuffCrazy stuff</Card.Text>
+                            <Card.Title>Another heading</Card.Title>
+                                <Card.Text>The text that goes with the above heading. Crazy stuff</Card.Text>
+                            <Card.Subtitle>A sub heading</Card.Subtitle>
+                                <Card.Text>Sub heading text</Card.Text>
+                        </Card.Body>
+                    </Card>
+
                 </Tab>
                 <Tab eventKey="edit" title="Edit">
                     <p>Edit tab</p>
