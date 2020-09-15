8a76096d9e8b761d05024662185313653e9967c7
Walrussuit101 <tjefferson@ycp.edu>
2020-09-14 20:18:54 -0400

    converted landing component to bootstrap

--- src/components/Wiki/WikiLanding.tsx
+++ src/components/Wiki/WikiLanding.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
 
+import {Card} from 'react-bootstrap';
+
 import './WikiLanding.css';
 
 class WikiLanding extends React.Component{
     render(){
         return(
-            <div>
-                <div id="statDiv">
-                    <h1>This wiki has</h1>
-                    <ul id="statList">
-                        <li>8 entries</li>
-                        <li>3 contributors</li>
-                        <li>26 comments</li>
-                        <li>12 images</li>
-                    </ul>
-                </div>
-                <div id="graphDiv">
-                    <h4>Graph of activity over time?</h4>
-                </div>
+            <div id="wikiLanding">
+                <Card bg="dark" text="white">
+                    <Card.Body>
+                        <Card.Title>This wiki has...</Card.Title>
+                        <ul id="statList">
+                            <li>8 entries</li>
+                            <li>3 contributors</li>
+                            <li>26 comments</li>
+                            <li>12 images</li>
+                        </ul>
+                    </Card.Body>
+                    <div id="graph">
+                        <p>Graph of activity over time?</p>
+                    </div>
+                </Card>
             </div>
         );
     }
