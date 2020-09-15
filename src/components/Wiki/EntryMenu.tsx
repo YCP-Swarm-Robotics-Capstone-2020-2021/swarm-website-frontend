774fb91c8708d385c24368142ff00370ea911782
Walrussuit101 <tjefferson@ycp.edu>
2020-09-14 20:02:22 -0400

    entry menu converted to bootstrap

--- src/components/Wiki/EntryMenu.tsx
+++ src/components/Wiki/EntryMenu.tsx
@@ -2,28 +2,25 @@ import React from 'react';
 
 import './EntryMenu.css';
 
+import {Card, ListGroup, ListGroupItem} from 'react-bootstrap';
+
 class EntryMenu extends React.Component{
     render(){
         return(
-            <div id='entryMenu'>
-                <div id='menuWikiTitle'>
-                    <h1>Simulation and Algorithm Design</h1>
-                </div>
-                <hr></hr>
-                <div id='menuEntries'>
-                    <ul id='entryList'>
-                        <li>Design</li>
-                        <li>Requirements</li>
-                        <li>1st Milestone</li>
-                        <li>2nd Milestone</li>
-                        <li>Final Milestone</li>
-                        <li>Technical Report</li>
-                    </ul>
-                </div>
-                <div id='lastUpdated'>
-                    <h4>Last Updated:</h4>
-                    <h4>9/7/2020</h4>
-                </div>
+            <div id="entryMenu">
+                <Card bg="dark" text="white">
+                    <Card.Body>
+                        <Card.Title>System Design and Analysis</Card.Title>
+                    </Card.Body>
+                    <ListGroup>
+                        <ListGroupItem variant="dark">Milestone 1</ListGroupItem>
+                        <ListGroupItem variant="dark">Milestone 2</ListGroupItem>
+                        <ListGroupItem variant="dark">Milestone 3</ListGroupItem>
+                        <ListGroupItem variant="dark">Milestone 4</ListGroupItem>
+                        <ListGroupItem variant="dark">Technical Report</ListGroupItem>
+                    </ListGroup>
+                    <Card.Footer className="text-center">Last Updated 10/23/2020</Card.Footer>
+                </Card>
             </div>
         );
     }
