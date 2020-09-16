e00be82b15150076d2aa570d7e3f15c624bebb66
Thomas McAdams <rithonn21@gmail.com>
2020-09-16 16:01:51 -0400

    Can now except form input into state

--- src/components/LogIn/LogIn.tsx
+++ src/components/LogIn/LogIn.tsx
@@ -10,9 +10,22 @@ import {Link} from "react-router-dom";
 const loginLogo = require('../../images/swarmLogoIcon.png');
 const background = backgroundImageStyling();
 
-class LogIn extends React.Component {
+interface UserLoginInfo {
+    loginInfo: {
+        email: string,
+        password: string
+    }
+}
+
+class LogIn extends React.Component<{}, UserLoginInfo> {
     constructor(props: any) {
         super(props);
+        this.state = {
+            loginInfo: {
+                email: '',
+                password: ''
+            }
+        };
     }
     componentDidMount() {
         setTimeout(function (){
@@ -20,6 +33,29 @@ class LogIn extends React.Component {
             document.getElementById('loginBox').classList.add('fade-in');
         }, 1)
     }
+
+    changeEmail = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        const {loginInfo} = this.state;
+        const {value} = event.currentTarget
+        const newLoginInfo = {
+            ...loginInfo,
+            email: value
+        };
+        this.setState({loginInfo: newLoginInfo})
+        console.log("this is the email: " + value);
+
+    }
+    changePassword = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        const {loginInfo} = this.state;
+        const {value} = event.currentTarget
+        const newLoginInfo = {
+            ...loginInfo,
+            password: value
+        };
+        this.setState({loginInfo: newLoginInfo})
+        console.log("this is the password: " + value);
+    }
+
 
     render() {
         return(
@@ -29,12 +65,15 @@ class LogIn extends React.Component {
                         <Image id='loginLogo' src={String(loginLogo)} alt='Swarm Robotics Logo'/>
                         <Form.Group controlId="formBasicEmail">
                             <Form.Text className={'text-center loginText'}>Email address</Form.Text>
-                            <Form.Control className={'loginTextInput text-center'} type="email" placeholder="Enter email"/>
+                            <Form.Control className={'loginTextInput text-center'} value={this.state.loginInfo.email}
+                                          onChange={this.changeEmail} type="email" placeholder="Enter email"/>
                         </Form.Group>
 
                         <Form.Group controlId="formBasicPassword">
                             <Form.Text className={'loginText text-center'}>Password</Form.Text>
-                            <Form.Control className={'loginTextInput text-center'} type="password" placeholder="Password" />
+                            <Form.Control className={'loginTextInput text-center'}
+                                          onChange={this.changePassword} value={this.state.loginInfo.password}
+                                          type="password" placeholder="Password" />
                         </Form.Group>
 
                         <Button id={'loginButton'} variant="primary" type="submit">
