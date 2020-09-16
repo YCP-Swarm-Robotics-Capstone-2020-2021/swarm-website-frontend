ca9b58bf8f3cfcd81f8817cbe29f834fab589184
cbrandt6 <cbrandt6@ycp.edu>
2020-09-15 23:24:14 -0400

    Created social media placeholder for sponsor page

--- src/components/PersonalPage/DevCard.tsx
+++ src/components/PersonalPage/DevCard.tsx
@@ -14,17 +14,22 @@ class DevCard extends React.Component{
             <Image id="profilePic" src={logo} rounded ></Image>
             <Card.Title id="profileName" style={{textAlign:"center"}}>Test Testineer</Card.Title>
             {/*If dev display this */}
-            <Card.Text id="gradYear" style={{textAlign:"center"}}>Graduating 2021 with a degree in Electrical Engineering</Card.Text>
+            <Card.Text id="gradYear" style={{textAlign:"center"}}>Graduating 2021 with a degree in Computer Science</Card.Text>
             <Card.Body>
                 
                 {/*Dev attributes*/}
+                <Card.Title id="devRoleHeader">My role in the project</Card.Title>
+                <Card.Text id="devRole">Software Engineer</Card.Text>
                 <Card.Title id="bioHeader">Biography</Card.Title>
-                <Card.Text id="bio">I am a fictional student studying Electrical Engineering at YCP.</Card.Text>
+                <Card.Text id="bio">I am a fictional student studying Computer Science from YCP.</Card.Text>
                 {/* If contributions is not null display this */}
                 <Card.Title id="contributionHeader">Here are some of my contributions</Card.Title>
-                <ListGroup id="contributions">
-                    <ListGroup.Item variant="Dark">
+                <ListGroup id="contributions" variant="flush">
+                    <ListGroup.Item id="listItem" variant="secondary">
                         <Card.Link className="contributionLink" href="#">My First Contribution</Card.Link>
+                    </ListGroup.Item>
+                    <ListGroup.Item id="listItem" variant="secondary">
+                        <Card.Link className="contributionLink" href="#">My Second Contribution</Card.Link>
                     </ListGroup.Item>
                 </ListGroup>
             </Card.Body>
--- src/components/PersonalPage/SponsorCard.tsx
+++ src/components/PersonalPage/SponsorCard.tsx
@@ -15,7 +15,17 @@ class SponsorCard extends React.Component{
                  <Card.Title id="companyMissionHeader">Our Mission Statement</Card.Title>
                 <Card.Text id="companyMission">We envision a world where our solutions advance healthcare and improve worker and patient safety. To make this world of difference, we leverage who we are as people and what we do as professionals to provide products when and where our customers need them.</Card.Text>
                 <Card.Title id="sponserReasonHeading">Why we chose to sponsor this project</Card.Title>
-                <Card.Text id="sponsorReason">We believe in the future for swarm robotics in the medical field. We see a future in using nanobot swarm injected into the human to help treat various diseases and injuries.</Card.Text>
+                <Card.Text id="sponsorReason">We believe in the future for swarm robotics in the medical field. We see a future in using nanobot swarm technology as an injection into the human to help treat various diseases and injuries.</Card.Text>
+                <Card.Title id="socialMediaHeader">Check us out on social media</Card.Title>
+                <ListGroup id="socialMedia" horizontal>
+                    <ListGroup.Item id="listItem" variant="secondary" style={{width: "6rem", height: "6rem"}}>
+                        <Card.Link className="contributionLink" href="#" ><Card.Img src={logo}></Card.Img></Card.Link>
+                    </ListGroup.Item>
+                    <ListGroup.Item id="listItem" variant="secondary" style={{width: "6rem", height: "6rem"}}>
+                        <Card.Link className="contributionLink" href="#" ><Card.Img src={logo}></Card.Img></Card.Link>
+                    </ListGroup.Item>
+                    
+                </ListGroup>
                 </Card.Body>
         </Card>
         );
