a0733ed9d68e25e3dc35b23837cb8bb18d4b3004
cbrandt6 <cbrandt6@ycp.edu>
2020-09-16 21:25:31 -0400

    Changed variables to props

--- src/components/PersonalPage/DevCard.tsx
+++ src/components/PersonalPage/DevCard.tsx
@@ -5,28 +5,39 @@ const swarmLogo = require('../../images/swarmLogoIcon.png');
 
 
 
-class DevCard extends React.Component{
+class DevCard extends React.Component<any, any>{
+    //Declaring props for the component
+    profileName: string;
+    gradYear: string;
+    degree: string;
+    teamRole: string;
+    bio: string;
+
+    //Setting the props
+    constructor(props: any){
+        super(props)
+        this.profileName = "Test Testineer";
+        this.gradYear = "2021";
+        this.degree = "Computer Science";
+        this.teamRole = "Software Engineer";
+        this.bio = "I am a fictional student studying Computer Science from YCP.";
+    }
 
     render(){
-        let profileName = "Test Testineer";
-        let gradYear = 2021;
-        let degree = "Computer Science";
-        let teamRole = "Software Engineer";
-        let bio = "I am a fictional student studying Computer Science from YCP.";
+        
         return(
         <Card id="profileCard" bg="dark" text="white">
             {/*Currently the src is the logo but will have to be changed to profile pic for engineer/sponsor*/}
             <Image id="profilePic" src={swarmLogo} rounded ></Image>
-        <Card.Title id="profileName" style={{textAlign:"center"}}>{profileName}</Card.Title>
-            {/*If dev display this */}
-            <Card.Text id="gradYear" style={{textAlign:"center"}}>Graduating in {gradYear} with a degree in {degree}</Card.Text>
+            <Card.Title id="profileName" style={{textAlign:"center"}}>{this.profileName}</Card.Title>
+            <Card.Text id="gradYear" style={{textAlign:"center"}}>Graduating in {this.gradYear} with a degree in {this.degree}</Card.Text>
             <Card.Body>
                 
                 {/*Dev attributes*/}
                 <Card.Title id="devRoleHeader">My role in the project</Card.Title>
-                <Card.Text id="devRole">{teamRole}</Card.Text>
+                <Card.Text id="devRole">{this.teamRole}</Card.Text>
                 <Card.Title id="bioHeader">A bit about me</Card.Title>
-                <Card.Text id="bio">{bio}</Card.Text>
+                <Card.Text id="bio">{this.bio}</Card.Text>
                 {/* If contributions is not null display this */}
                 <Card.Title id="contributionHeader">Here are some of my contributions</Card.Title>
                 <ListGroup id="contributions" variant="flush">
--- src/components/PersonalPage/SponsorCard.tsx
+++ src/components/PersonalPage/SponsorCard.tsx
@@ -7,32 +7,44 @@ const twitterLogo = require('../../images/socialMediaIcons/Twitter_Social_Icon_R
 
 const swarmLogo = require('../../images/swarmLogoIcon.png');
 
-class SponsorCard extends React.Component{
+class SponsorCard extends React.Component<any, any>{
+    //Declaring all props for the component
+    companyName: string;
+    companyMission: string;
+    sponsorReason: string;
+    twitterLink: string;
+    instagramLink: string;
+    
+    // Setting prop values
+    constructor(props: any){
+        super(props);
+        this.companyName = "Becton Dickinson";
+        this.companyMission = "We envision a world where our solutions advance healthcare and improve worker and patient safety. To make this world of difference, we leverage who we are as people and what we do as professionals to provide products when and where our customers need them.";
+        this.sponsorReason = "We believe in the future for swarm robotics in the medical field. We see a future in using nanobot swarm technology as an injection into the human to help treat various diseases and injuries.";
+        this.twitterLink = "#";
+        this.instagramLink = "#";
+    }
     
     render(){
-        let companyName = "Becton Dickinson";
-        let companyMission = "We envision a world where our solutions advance healthcare and improve worker and patient safety. To make this world of difference, we leverage who we are as people and what we do as professionals to provide products when and where our customers need them.";
-        let sponsorReason = "We believe in the future for swarm robotics in the medical field. We see a future in using nanobot swarm technology as an injection into the human to help treat various diseases and injuries.";
-        let twitterLink = "#";
-        let instagramLink = "#";
+        
 
         return(
         <Card id="profileCard" bg="dark" text="white">
             <Image id="companyPic" src={companyLogo} rounded ></Image>
-            <Card.Title id="companyName" style={{textAlign:"center"}}>{companyName}</Card.Title>
+            <Card.Title id="companyName" style={{textAlign:"center"}}>{this.companyName}</Card.Title>
             <Card.Body>
                  {/*Sponsor attributes*/}
                  <Card.Title id="companyMissionHeader">Our Mission Statement</Card.Title>
-                <Card.Text id="companyMission">{companyMission}</Card.Text>
+                <Card.Text id="companyMission">{this.companyMission}</Card.Text>
                 <Card.Title id="sponserReasonHeading">Why we chose to sponsor this project</Card.Title>
-                <Card.Text id="sponsorReason">{sponsorReason}</Card.Text>
+                <Card.Text id="sponsorReason">{this.sponsorReason}</Card.Text>
                 <Card.Title id="socialMediaHeader">Check us out on social media</Card.Title>
                 <ListGroup id="socialMedia" horizontal>
                     <ListGroup.Item id="listItem" variant="secondary" style={{width: "6rem", height: "6rem"}}>
-                        <Card.Link className="twitterLink" href={twitterLink} ><Card.Img src={twitterLogo}></Card.Img></Card.Link>
+                        <Card.Link className="twitterLink" href={this.twitterLink} ><Card.Img src={twitterLogo}></Card.Img></Card.Link>
                     </ListGroup.Item>
                     <ListGroup.Item id="listItem" variant="secondary" style={{width: "6rem", height: "6rem"}}>
-                        <Card.Link className="instagramLink" href={instagramLink} ><Card.Img src={instagramLogo} style={{filter: "invert(100%)", backgroundColor: "black", borderRadius: "7px"}}></Card.Img></Card.Link>
+                        <Card.Link className="instagramLink" href={this.instagramLink} ><Card.Img src={instagramLogo} style={{filter: "invert(100%)", backgroundColor: "black", borderRadius: "7px"}}></Card.Img></Card.Link>
                     </ListGroup.Item>
                     
                 </ListGroup>
