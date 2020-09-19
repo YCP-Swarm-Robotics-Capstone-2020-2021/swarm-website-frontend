cc2e15e5e244c97a6d196df9cf2ca55e491abbcc
Walrussuit101 <tjefferson@ycp.edu>
2020-09-18 23:15:11 -0400

    fix typing in EntryMenu

--- src/components/Wiki/EntryMenu.tsx
+++ src/components/Wiki/EntryMenu.tsx
@@ -4,7 +4,11 @@ import './EntryMenu.css';
 
 import {Card, ListGroup, ListGroupItem} from 'react-bootstrap';
 
-class EntryMenu extends React.Component<any, any>{
+interface entryMenuProps{
+    action: (entryId: string) => void
+}
+
+class EntryMenu extends React.Component<entryMenuProps, {}>{
     render(){
         return(
             <div id="entryMenu">
@@ -13,11 +17,11 @@ class EntryMenu extends React.Component<any, any>{
                         <Card.Title onClick={() => this.props.action("landing")}>System Design and Analysis</Card.Title>
                     </Card.Body>
                     <ListGroup>
-                        <ListGroupItem onClick={() => this.props.action(1)} variant="dark">Milestone 1</ListGroupItem>
-                        <ListGroupItem onClick={() => this.props.action(2)} variant="dark">Milestone 2</ListGroupItem>
-                        <ListGroupItem onClick={() => this.props.action(3)} variant="dark">Milestone 3</ListGroupItem>
-                        <ListGroupItem onClick={() => this.props.action(4)} variant="dark">Milestone 4</ListGroupItem>
-                        <ListGroupItem onClick={() => this.props.action(5)} variant="dark">Technical Report</ListGroupItem>
+                        <ListGroupItem onClick={() => this.props.action("1")} variant="dark">Milestone 1</ListGroupItem>
+                        <ListGroupItem onClick={() => this.props.action("2")} variant="dark">Milestone 2</ListGroupItem>
+                        <ListGroupItem onClick={() => this.props.action("3")} variant="dark">Milestone 3</ListGroupItem>
+                        <ListGroupItem onClick={() => this.props.action("4")} variant="dark">Milestone 4</ListGroupItem>
+                        <ListGroupItem onClick={() => this.props.action("5")} variant="dark">Technical Report</ListGroupItem>
                     </ListGroup>
                     <Card.Footer className="text-center">Last Updated 10/23/2020</Card.Footer>
                 </Card>
