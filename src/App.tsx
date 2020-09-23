a9f756ef9c04ccfdb908be1e1f5a2f16e707c744
Thomas McAdams <rithonn21@gmail.com>
2020-09-23 10:25:06 -0400

    Fixed routing conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,8 @@ import LogIn from './components/LogIn/LogIn';
 import Wiki from './components/Wiki/Wiki';
 import HomePage from "./components/Home/HomePage";
 import Error from './components/Error/Error';
-<<<<<<< HEAD
- import SignUp from "./components/SignUp/SignUp";
-=======
+import SignUp from "./components/SignUp/SignUp";
 import PersonalPage from './components/PersonalPage/PersonalPage';
->>>>>>> 037dc9cf85889aaef926a378a857826570e5281a
 
 function  App(){
     return(
@@ -19,11 +16,8 @@ function  App(){
                 <Route path='/' component={LogIn} exact/>
                 <Route path='/wiki/:id' component={Wiki} />
                 <Route path='/home' component={HomePage} exact/>
-<<<<<<< HEAD
                 <Route path='/signUp' component={SignUp}/>
-=======
                 <Route path='/personalpage' component={PersonalPage} exact/>
->>>>>>> 037dc9cf85889aaef926a378a857826570e5281a
                 <Route component={Error} />
             </Switch>
         </main>
