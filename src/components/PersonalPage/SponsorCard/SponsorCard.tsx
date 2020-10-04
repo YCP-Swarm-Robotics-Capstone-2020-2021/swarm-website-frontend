70d7fed6cf1673acef74a01ea5ef40260a60cb3b
cbrandt6 <cbrandt6@ycp.edu>
2020-10-03 22:33:10 -0400

    Created fetch request for sponsor

--- src/components/PersonalPage/SponsorCard/SponsorCard.tsx
+++ src/components/PersonalPage/SponsorCard/SponsorCard.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import {Card, Image, ListGroup} from 'react-bootstrap';
+import {userProps, personalPage} from "../PersonalPage";
 import "./SponsorCard.css"
+// @ts-ignore, it doesn't like require
 const companyLogo = require('../../../images/bd.svg');
+// @ts-ignore, it doesn't like require
 const instagramLogo = require('../../../images/socialMediaIcons/Instagram_Glyph_White-2.svg');
+// @ts-ignore, it doesn't like require
 const twitterLogo = require('../../../images/socialMediaIcons/Twitter_Social_Icon_Rounded_Square_White.svg');
 
+// @ts-ignore, it doesn't like require
 const swarmLogo = require('../../../images/swarmLogoIcon.png');
 
-/*
-    The Sponsor will be passed in as the prop as it provides basic info including id which
-    can be used to query the rest of the info needed for the page
-*/
-
-interface SponsorPage {
-    SponsorPage: {
-     //Declaring all props for the component
+interface sponsorPage {
+    sponsorPage: {
     companyName: string;
     companyMission: string;
     sponsorReason: string;
@@ -23,25 +22,78 @@ interface SponsorPage {
     }
 }
 
-class SponsorCard extends React.Component<{}, SponsorPage>{
-    
-    // Setting prop values
-    constructor(props: SponsorPage){
+interface sponsor {
+    sponsor: {
+        id: string,
+        username: string,
+        password?: string | null,
+        email: string,
+        firstName: string,
+        lastName: string,
+        companyName: string,
+        page: string
+    }
+}
+
+
+class SponsorCard extends React.Component<userProps & personalPage, sponsorPage & sponsor>{
+    constructor(props: personalPage & userProps){
         super(props);
-        this.state ={
-            SponsorPage: {
-                companyName: "Becton Dickinson",
-                companyMission:"We envision a world where our solutions advance healthcare and improve worker and patient safety. To make this world of difference, we leverage who we are as people and what we do as professionals to provide products when and where our customers need them.",
-                sponsorReason: "We believe in the future for swarm robotics in the medical field. We see a future in using nanobot swarm technology as an injection into the human to help treat various diseases and injuries.",
+        this.state = {
+            sponsorPage: {
+                companyName: "",
+                companyMission:"",
+                sponsorReason: "",
                 twitterLink: "#",
                 instagramLink: "#"
             
-        }
+            },
+
+            sponsor: {
+                id: '',
+                username: '',
+                password: null,
+                email: '',
+                firstName: '',
+                lastName: '',
+                companyName: '',
+                page: ''
+
+            }
+
         
     };
 }
     //TODO write componentDidMount
-    //Should make requests for user info
+    onComponentDidMount() {
+        //Should make requests for sponsor info
+        let sponsorID = this.props.user.id;
+        fetch('http://localhost:8000/sponsor/' + sponsorID + '/',{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    sponsor: {
+                        id: data['id'],
+                        username: data['username'],
+                        password: null,
+                        email: data['email'],
+                        firstName: data['firstName'],
+                        lastName: data['lastName'],
+                        companyName: data['companyName'],
+                        page: data['page']
+                    }
+                })
+            })
+
+        //TODO request sponsor page details
+    }
+
+
 
     //TODO Create fetch request to retrieve devpage info
     
@@ -49,22 +101,22 @@ class SponsorCard extends React.Component<{}, SponsorPage>{
         return(
         <Card id="profileCard" bg="dark" text="white">
             <Image id="companyPic" src={companyLogo} rounded ></Image>
-            <Card.Title id="companyName">{this.state.SponsorPage.companyName}</Card.Title>
+            <Card.Title id="companyName">{this.state.sponsorPage.companyName}</Card.Title>
             <Card.Body>
                  {/*Sponsor attributes*/}
                  <Card.Title id="companyMissionHeader">Our Mission Statement</Card.Title>
-                <Card.Text id="companyMission">{this.state.SponsorPage.companyMission}</Card.Text>
+                <Card.Text id="companyMission">{this.state.sponsorPage.companyMission}</Card.Text>
                 <Card.Title id="sponserReasonHeading">Why we chose to sponsor this project</Card.Title>
-                <Card.Text id="sponsorReason">{this.state.SponsorPage.sponsorReason}</Card.Text>
+                <Card.Text id="sponsorReason">{this.state.sponsorPage.sponsorReason}</Card.Text>
                 <Card.Title id="socialMediaHeader">Check us out on social media</Card.Title>
                 <ListGroup id="socialMedia" horizontal>
                     <ListGroup.Item id="sponsorListItem" variant="secondary">
-                        <Card.Link id="socialMediaLink" href={this.state.SponsorPage.twitterLink} >
+                        <Card.Link id="socialMediaLink" href={this.state.sponsorPage.twitterLink} >
                             <Card.Img id="twitterIcon" src={twitterLogo}></Card.Img>
                         </Card.Link>
                     </ListGroup.Item>
                     <ListGroup.Item id="sponsorListItem" variant="secondary">
-                        <Card.Link id="socialMediaLink" href={this.state.SponsorPage.instagramLink} >
+                        <Card.Link id="socialMediaLink" href={this.state.sponsorPage.instagramLink} >
                             <Card.Img id="instagramIcon" src={instagramLogo}></Card.Img>
                         </Card.Link>
                     </ListGroup.Item>
