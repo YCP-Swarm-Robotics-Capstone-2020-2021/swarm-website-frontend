c0e06f16371507a87503913ec9ef47206cd2ed45
cbrandt6 <cbrandt6@ycp.edu>
2020-10-06 10:23:30 -0400

    Moved user request to respective cards

--- src/components/PersonalPage/PersonalPage.tsx
+++ src/components/PersonalPage/PersonalPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// @ts-ignore
+
 import {RouteComponentProps} from "react-router";
 
 import './PersonalPage.css';
@@ -17,6 +17,7 @@ const background = backgroundImageStyling();
 
 interface personalPageProps extends RouteComponentProps<{id: string}> {}
 
+
 //Define personal page interface to be used with state
 export interface personalPage {
     personalPage: {
@@ -33,7 +34,8 @@ interface card {
     }
 }
 
-export interface userProps {
+//Exported interface for a base user
+export interface user {
     user: {
         id: string,
         username: string,
@@ -44,9 +46,10 @@ export interface userProps {
     }
 }
 
-class PersonalPage extends React.Component<RouteComponentProps<{id: string}> & userProps, personalPage & card>{
-    constructor(props: RouteComponentProps<{id: string}> & userProps){
+class PersonalPage extends React.Component<RouteComponentProps<{id: string}>, personalPage & card & user>{
+    constructor(props: RouteComponentProps<{id: string}>){
         super(props);
+        //Initialize state and fields to empty
         this.state = {
             personalPage: {
                 id: '',
@@ -55,8 +58,17 @@ class PersonalPage extends React.Component<RouteComponentProps<{id: string}> & u
             },
             card: {
                 cardType: null,
+            },
+            user: {
+                id: '',
+                username: '',
+                password: '',
+                email: '',
+                firstName: '',
+                lastName: '',
             }
         }
+        //Bind card type function
         this.setCardType = this.setCardType.bind(this);
     }
 
@@ -66,44 +78,87 @@ class PersonalPage extends React.Component<RouteComponentProps<{id: string}> & u
             document.getElementById('cardHolder').classList.add('fade-in');
         }, 1)
 
-        let id = this.props.match.params.id;
-        fetch("http://localhost:8000/personalpage/"+id+'/', {
+        //Create request for personalpage information using parameter id
+        let pageID = this.props.match.params.id;
+        console.log("Page id: " + pageID);
+        fetch("http://localhost:8000/personalpage/"+pageID, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
             }
         })
+            //Parse response into json
             .then(response => response.json())
+            //Set state using json data
             .then(data => {
+                console.log(data);
                 this.setState({
                     personalPage: {
                         id: data['id'],
                         pageType: data['pageType'],
                         pageTitle: data['pageTitle']
-                    }
-                })
+                    },
+                    user: this.state.user
+                });
+                console.log("Page type: " + this.state.personalPage.pageType)
+                // //http://localhost:8000/developer/?page=1
+                // if(this.state.personalPage.pageType === "Developer"){
+                //     console.log('querying for developer based on page ' + this.state.personalPage.id)
+                //     fetch("http://localhost:8000/developer/?page=" + this.state.personalPage.id.toString(), {
+                //         method: 'GET',
+                //         headers: {
+                //             'Content-Type': 'application/json'
+                //         }
+                //     })
+                //         .then(response => response.json())
+                //         .then(data => {
+                //             console.log(data);
+                //             this.setState({
+                //                 user: {
+                //                     id: data['id'],
+                //                     username: data['username'],
+                //                     password: '',
+                //                     email: data['email'],
+                //                     firstName: data['firstName'],
+                //                     lastName: data['lastName'],
+                //                 },
+                //                 personalPage: this.state.personalPage
+                //             });
+                //             console.log("User: " + this.state.user);
+                //             //Set the card type in the promise so it actually executes
+                //             this.setCardType();
+                //
+                //         })
+                // }else if(this.state.personalPage.pageType === "Sponsor"){
+                //
+                // }
                 this.setCardType();
-            });        
+            });
+
+
+
     }
 
+    //Function to set the card type based on the type of the page
     setCardType(){
+        //If page type is developer create set card to developer
         if(this.state.personalPage.pageType === "Developer"){
-
             this.setState({
                 card: {
-                    cardType: <DevCard personalPage={this.state.personalPage} user={this.props.userProps}/>
+                    cardType: <DevCard personalPage={this.state.personalPage} />
                 }
             })
         }else if(this.state.personalPage.pageType === "Sponsor"){
             this.setState({
                 card: {
-                    cardType: <SponsorCard personalPage={this.state.personalPage} user={this.props.userProps}/>
+                    cardType: <SponsorCard personalPage={this.state.personalPage} user={this.state.user}/>
                 }
             })
         }
     }
     
     render() {
+
         return(
             <section style={background}>
                 <div id='navbarHolder'>
