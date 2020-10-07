e68bb53edbf5e882d7a4c58efdb14296e7e9cf60
Rithonn <rithonn21@gmail.com>
2020-10-07 10:39:11 -0400

    Working fetch request for user login info

--- src/components/LogIn/LogIn.tsx
+++ src/components/LogIn/LogIn.tsx
@@ -4,6 +4,7 @@ import {Button, Form} from "react-bootstrap";
 import './LogIn.css';
 import Image from "../../utils/Image";
 import {Link} from "react-router-dom";
+import {verifyUser} from "./LoginApi";
 
 
 //require any images
@@ -17,21 +18,11 @@ interface UserLoginInfo {
     }
 }
 
-async function postData(url = '', data = {}){
-    const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    });
-    return response.json();
-}
 
-class LogIn extends React.Component<{}, UserLoginInfo> {
+class LogIn extends React.Component<{}> {
     state = {
         loginInfo:{
-            email: '',
+            username: '',
             password: ''
         }
     }
@@ -42,29 +33,43 @@ class LogIn extends React.Component<{}, UserLoginInfo> {
         }, 1)
     }
 
+    handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        const {loginInfo} = this.state;
+        const newLoginInfo = {
+            ...loginInfo,
+            [e.target.name]: e.target.value
+        }
+        this.setState({loginInfo: newLoginInfo})
+        console.log(this.state.loginInfo)
+    }
+
+    handleSubmit = (e: React.FormEvent): void => {
+        verifyUser(this.state.loginInfo.username, this.state.loginInfo.password)
+        e.preventDefault();
+    }
 
     render() {
         return(
             <section style={background}>
                 <div id="loginBox">
-                    <Form>
+                    <Form onSubmit={this.handleSubmit}>
                         <Image id='loginLogo' src={String(loginLogo)} alt='Swarm Robotics Logo'/>
                         <Form.Group controlId="formBasicEmail">
-                            <Form.Text className={'text-center loginText'}>Email address</Form.Text>
-                            <Form.Control className={'loginTextInput text-center'} value={this.state.loginInfo.email}
-                                          onChange={this.changeEmail} type="email"
-                                          required placeholder="Enter email"/>
+                            <Form.Text className={'text-center loginText'}>Username</Form.Text>
+                            <Form.Control className={'loginTextInput text-center'}
+                                          onChange={this.handleChange} type="username"
+                                          name={"username"} required placeholder="Enter Username"/>
                         </Form.Group>
 
                         <Form.Group controlId="formBasicPassword">
                             <Form.Text className={'loginText text-center'}>Password</Form.Text>
                             <Form.Control className={'loginTextInput text-center'}
-                                          onChange={this.changePassword} value={this.state.loginInfo.password}
-                                          required type="password"
+                                          onChange={this.handleChange}
+                                          required type="password" name={"password"}
                                           placeholder="Password" />
                         </Form.Group>
 
-                        <Button id={'loginButton'} variant="primary" type="submit" onClick={this.submitInfo}>
+                        <Button id={'loginButton'} variant="primary" type="submit">
                             Login
                         </Button>
 
@@ -80,35 +85,6 @@ class LogIn extends React.Component<{}, UserLoginInfo> {
                 </div>
             </section>
         );
-    }
-    //These methods serve the purpose of updating state input from the user
-    changeEmail = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        const {loginInfo} = this.state;
-        const {value} = event.currentTarget
-        const newLoginInfo = {
-            ...loginInfo,
-            email: value
-        };
-        this.setState({loginInfo: newLoginInfo})
-    }
-    //These methods serve the purpose of updating state input from the user
-    changePassword = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        const {loginInfo} = this.state;
-        const {value} = event.currentTarget
-        const newLoginInfo = {
-            ...loginInfo,
-            password: value
-        };
-        this.setState({loginInfo: newLoginInfo})
-    }
-    submitInfo = (event: React.MouseEvent<HTMLButtonElement>): void => {
-        postData('https://localhost:8000/login', {loginInfo: this.state.loginInfo})
-            .then(data => {
-                console.log(data);
-            }).catch(
-                error => alert(error.message)
-            );
-        console.log(this.state.loginInfo);
     }
 }
 
