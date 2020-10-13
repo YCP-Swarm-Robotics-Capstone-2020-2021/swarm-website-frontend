f8bbc2eb1cb7b576f89a972d52fb42ec09b097d5
Rithonn <rithonn21@gmail.com>
2020-10-12 21:16:18 -0400

    User handleChangeEvent

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -25,6 +25,10 @@ interface SignUpState{
 
 interface SignUpProps extends RouteComponentProps<{}>{}
 
+function UserAlreadyExists(){
+    return <p>User already exists</p>
+}
+
 class SignUp extends React.Component<SignUpProps, SignUpState> {
 
     constructor(props: any) {
@@ -50,36 +54,77 @@ class SignUp extends React.Component<SignUpProps, SignUpState> {
         }, 1)
     }
 
+    handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        const signUp = this.state.data;
+        const signUpData = {
+            ...signUp,
+            [e.target.name]: e.target.value
+        }
+        this.setState({data: signUpData});
+        console.log(this.state.data);
+    }
+
+    handleSubmit = async (e: React.FormEvent)=> {
+        e.preventDefault();
+
+        //First make sure the user doesn't exist
+
+        const response = await verifyUser(this.state.data.username, this.state.data.password);
+        if(response.Status){
+            return <UserAlreadyExists></UserAlreadyExists>
+        }
+
+
+        //Create user and redirect to login
+        return "yay"
+    }
+
     render() {
         return(
             <section style={background}>
                 <div id="signUpBox">
                     <Form>
                         <Image id='signUplogo' src={String(logo)} alt='Swarm Robotics Logo'/>
+
                         <Form.Group controlId="formBasicEmail">
                             <Form.Text className={'text-center signUpText'}>First Name</Form.Text>
-                            <Form.Control className={'signUpTextInput text-center'} type="email" placeholder="First Name" />
+                            <Form.Control className={'signUpTextInput text-center'}
+                                          name={"firstName"} onChange={this.handleChange}
+                                          type="firstName" placeholder="First Name" />
                         </Form.Group>
+
                         <Form.Group controlId="formBasicEmail">
                             <Form.Text className={'text-center signUpText'}>Last Name</Form.Text>
-                            <Form.Control className={'signUpTextInput text-center'} type="email" placeholder="Last Name" />
+                            <Form.Control className={'signUpTextInput text-center'}
+                                          name={"lastName"} onChange={this.handleChange}
+                                          type="lastName" placeholder="Last Name" />
                         </Form.Group>
+
                         <Form.Group controlId="formBasicEmail">
                             <Form.Text className={'text-center signUpText'}>Email address</Form.Text>
-                            <Form.Control className={'signUpTextInput text-center'} type="email" placeholder="Enter email" />
+                            <Form.Control className={'signUpTextInput text-center'}
+                                          name={"email"} onChange={this.handleChange}
+                                          type="email" placeholder="Enter email" />
                         </Form.Group>
 
                         <Form.Group controlId="formBasicPassword">
                             <Form.Text className={'signUpText text-center'}>Password</Form.Text>
-                            <Form.Control className={'signUpTextInput text-center'} type="password" placeholder="Password" />
+                            <Form.Control className={'signUpTextInput text-center'}
+                                          name={"password"} onChange={this.handleChange}
+                                          type="password" placeholder="Password" />
                         </Form.Group>
+
                         <Form.Group controlId="formBasicPassword">
                             <Form.Text className={'signUpText text-center'}>Verify Password</Form.Text>
-                            <Form.Control className={'signUpTextInput text-center'} type="password" placeholder="Verify Passowrd" />
+                            <Form.Control className={'signUpTextInput text-center'}
+                                          name={"verifyPassword"} onChange={this.handleChange}
+                                          type="password" placeholder="Verify Passowrd" />
                         </Form.Group>
+
                         <Button id={'signUpButton'} variant="primary" type="submit">
                             Sign Up
                         </Button>
+
                         <Link to="/">
                             <Form.Text className={"text-muted text-center"}>
                                 Login
