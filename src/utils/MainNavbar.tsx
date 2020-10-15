b0ab0f87ec0d55ec33c2600a4b8805a0d7ce863a
Walrussuit101 <tjefferson@ycp.edu>
2020-10-14 21:15:44 -0400

    add dropdown in navbar for wikis
    link to /wiki/{id} route

--- src/utils/MainNavbar.tsx
+++ src/utils/MainNavbar.tsx
@@ -1,12 +1,41 @@
 import React from "react";
-import {Nav, Navbar} from "react-bootstrap";
+import {Nav, Navbar, Dropdown} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import {wikiData} from "./getInterfaces/wikiData";
 
 interface Props{
     logo: string;
 }
 
-class MainNavbar extends React.Component<Props>{
+interface State{
+    wikiDropdownItems:  JSX.Element[]
+}
+
+class MainNavbar extends React.Component<Props, State>{
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            wikiDropdownItems: []
+        }
+    }
+    componentDidMount() {
+        fetch("http://localhost:8000/wiki/", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                data.forEach((wiki: wikiData) => {
+                    this.setState({wikiDropdownItems: this.state.wikiDropdownItems.concat(
+                            <Dropdown.Item key={wiki.id} href={"/wiki/"+wiki.id}>{wiki.title}</Dropdown.Item>
+                        )
+                    })
+                })
+            })
+    }
+
     render(){
         return(
             <>
@@ -27,8 +56,17 @@ class MainNavbar extends React.Component<Props>{
                         <Nav className="mr-auto">
                             <Nav.Link href="/">Home</Nav.Link>
                             <Nav.Link href="#home">Visualization</Nav.Link>
-                            <Nav.Link href="#home">Wiki</Nav.Link>
                             <Nav.Link href="#home">Gallery</Nav.Link>
+                            <Dropdown id="wikiDropdown">
+                                <Dropdown.Toggle variant="success">
+                                    Wikis
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    {this.state.wikiDropdownItems}
+                                    <Dropdown.Divider />
+                                    <Dropdown.Item>Add Wiki</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
 
                         </Nav>
                     </Navbar.Collapse>
