708ca96ee94a2adc837f3b184d098956e7b7af1d
Thomas McAdams <rithonn21@gmail.com>
2020-10-24 11:29:09 -0400

    Adding landing page text

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import './HomePage.css';
 import backgroundImageStyling from '../../styles/backgroundImageStyling';
 import MainNavbar from "../../utils/MainNavbar";
+import { Button } from 'react-bootstrap';
 
 //require any images
 const logo = require('../../images/swarmLogoIcon.png');
@@ -10,12 +11,26 @@ const logo = require('../../images/swarmLogoIcon.png');
 const background = backgroundImageStyling();
 //const styleSheet = styles();
 
+
 class HomePage extends React.Component{
 
     render(){
         return(
             <section style={background}>
                 <MainNavbar logo={logo}></MainNavbar>
+
+                <div id={'missionStatement'}>
+                    <div >
+                        <h1 className={'fontLarge'}>Automation</h1>
+                        <h1 className={'fontLarge'}>Execution</h1>
+                        <h3 className={'fontMedium'}>For Controlled Environments</h3>
+                    </div>
+                    <div id={'missionStatementButton'}>
+                        <Button id={'visualizationRedirectButton'}>Visualization</Button>{' '}
+                    </div>
+                </div>
+
+
             </section>
         );
     }
