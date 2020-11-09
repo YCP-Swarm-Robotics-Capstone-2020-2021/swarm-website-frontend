883ee514bcfbe717970987a69de700963c53583d
Rithonn <rithonn21@gmail.com>
2020-11-08 20:02:21 -0500

    Canvas is there

--- src/components/Visualization/Visualization.tsx
+++ src/components/Visualization/Visualization.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import backgroundImageStyling from '../../styles/backgroundImageStyling'
-import {Button, Form} from "react-bootstrap";
-import Image from "../../utils/Image";
-import {Link, RouteComponentProps} from "react-router-dom";
-import MainNavbar from "../../utils/MainNavbar";
-const logo = require('../../images/swarmLogoIcon.png');
 
+import MainNavbar from "../../utils/MainNavbar";
+
+const logo = require('../../images/swarmLogoIcon.png');
 const background = backgroundImageStyling();
 
 
 
-
 class Visualization extends React.Component<any, any>{
+
+
     render() {
         return (
             <section>
-                <MainNavbar logo={logo}></MainNavbar>
+                <MainNavbar logo={logo}/>
                 <p>Hi there</p>
+                <canvas id="canvas" tabIndex={1} width="1280" height="720"/>
             </section>
 
         );
