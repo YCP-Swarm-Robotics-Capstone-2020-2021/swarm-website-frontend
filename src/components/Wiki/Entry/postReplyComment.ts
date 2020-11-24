a4a79014a88e3939820708d39c1367c0b0d39ac7
Walrussuit101 <tjefferson@ycp.edu>
2020-11-24 14:30:46 -0500

    also need entry #

--- src/components/Wiki/Entry/postReplyComment.ts
+++ src/components/Wiki/Entry/postReplyComment.ts
@@ -1,7 +1,7 @@
 import {url} from "../../../utils/DetermineUrl";
 import {newCommentData} from "../../../utils/postInterfaces/newCommentData";
 
-export function postReplyComment(commentQuote: string, comment: newCommentData){
+export function postReplyComment(commentQuote: string, comment: newCommentData, entryId: number){
     console.log("commentQuote: "+commentQuote);
     console.log("new comment: "+comment.user + ": "+comment.text);
 }
