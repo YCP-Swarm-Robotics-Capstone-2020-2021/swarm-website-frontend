1c49753b5f5fd63baafa47678e466a0d1309f57b
cbrandt6 <cbrandt6@ycp.edu>
2021-02-03 11:03:49 -0500

    Appended /api and put in actual production url

--- src/utils/DetermineUrl.ts
+++ src/utils/DetermineUrl.ts
@@ -2,9 +2,9 @@ export const url = handleUrl();
 
 function handleUrl() {
     if(process.env.NODE_ENV=== 'development'){
-        return 'http://localhost:8000'
+        return 'http://localhost:8000/api'
     }
     if(process.env.NODE_ENV === 'production'){
-        return 'productionurl'
+        return 'http://swarmrobotics.io/api'
     }
 }
