1c95384ea74c4526db7b97c7602f0117b777b37a
cbrandt6 <cbrandt6@ycp.edu>
2021-04-11 14:49:55 -0400

    removed unused imports

--- src/components/Log/Log.tsx
+++ src/components/Log/Log.tsx
@@ -3,15 +3,12 @@ import React from 'react';
 import './Log.css';
 import backgroundImageStyling from '../../styles/backgroundImageStyling';
 import MainNavbar from "../../utils/MainNavbar";
-import {RouteComponentProps} from "react-router-dom";
-import verifyUserIsLoggedIn from "../../utils/verifiyUserIsLoggedIn/verifyLoggedIn";
-import { ReactComponent } from '*.svg';
 
 const logo = require('../../images/swarmLogoIcon.png');
 
 const background = backgroundImageStyling();
 
-interface LogProps extends RouteComponentProps<{}>{
+interface LogProps{
 
 }
 
