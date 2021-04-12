18ba75569f4d9fd565a28e28db47eba9f7a3e26c
cbrandt6 <cbrandt6@ycp.edu>
2021-04-11 20:23:54 -0400

    Changed props to be a LogStruct

--- src/components/Log/LogRow.tsx
+++ src/components/Log/LogRow.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
 
 import './LogRow.css';
+import LogStruct from './LogStruct'
+import {ListGroup} from 'react-bootstrap';
 
-interface LogRowProps{
-
-}
-
-interface LogRowState{
-
-}
-
-
-class LogRow extends React.Component<LogRowProps, LogRowState>{
-    constructor(props: LogRowProps){
+class LogRow extends React.Component<LogStruct, {}>{
+    constructor(props: LogStruct){
         super(props);
     }
 
     render(){
         return(
-            <p>Hello There</p>
+            <ListGroup horizontal>
+                <ListGroup.Item id="topRow" variant="dark">{this.props.deviceID}</ListGroup.Item>
+                <ListGroup.Item id="topRow" variant="dark">{this.props.dateTime}</ListGroup.Item>
+            </ListGroup>
         )
     }
 }
