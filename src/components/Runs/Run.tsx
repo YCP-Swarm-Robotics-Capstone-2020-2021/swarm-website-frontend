26cfe7b99f5c0db3a6fdf7fb94f99a378b237e93
Walrussuit101 <tjefferson@ycp.edu>
2021-04-20 11:46:16 -0400

    <Run/> fetches data based on props.id

--- src/components/Runs/Run.tsx
+++ src/components/Runs/Run.tsx
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {Container, Row, Col, Spinner} from "react-bootstrap";
+import {url} from "../../utils/DetermineUrl";
+import runData from "../../utils/getInterfaces/runData";
 
 import './styles/Run.css';
 
@@ -10,14 +12,23 @@ interface runProps {
 
 const Run: React.FC<runProps> = (props: runProps) => {
     const [loading, setLoading] = useState(true);
+    const [data, setData] = useState<runData>({id: 0, dateTime: '', deviceID: '', runID: 0, logId: 0});
 
     useEffect(() => {
         const load = async () => {
             //TOOD: fetch run data from /api/run/{props.id} or /api/run?id={props.id}
-            //set a placeholder timeout to show loading spinner
-            setTimeout(() => {
-                setLoading(false);
-            }, 1500)
+            const response = await fetch(url+'/run?id='+props.id, {
+                method: 'GET',
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if(!response.ok) return console.log('Fetching run data failed...');
+            const json = await response.json();
+
+            setData(json[0] as runData);
+            setLoading(false);
         }
         load();
     }, [])
@@ -27,7 +38,7 @@ const Run: React.FC<runProps> = (props: runProps) => {
             <Row className={'justify-content-center'}>
                 <Col className={'text-center'}>
                     {
-                        loading ? <Spinner animation={'border'}/> : <h3>{props.id}</h3>
+                        loading ? <Spinner animation={'border'}/> : <h3>{data.runID + data.deviceID + data.dateTime}</h3>
                     }
                 </Col>
             </Row>
