6dd6f6b9f30826c292eca7596bf98754fe57874e
Walrussuit101 <tjefferson@ycp.edu>
2021-04-20 23:18:58 -0400

    use /run/get_run_json route to load run with json/display it

--- src/components/Runs/Run.tsx
+++ src/components/Runs/Run.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
-import {Container, Row, Col, Spinner, Button} from "react-bootstrap";
+import {Container, Row, Col, Spinner, Button, InputGroup, FormControl, Form} from "react-bootstrap";
 import {url} from "../../utils/DetermineUrl";
-import runData from "../../utils/getInterfaces/runData";
+import runWithJSONData from '../../utils/getInterfaces/runWithJSONData';
 
 import './styles/Run.css';
 
@@ -10,14 +10,17 @@ interface runProps {
     changeView: (view: string) => void
 }
 
+const loadingSpinner = <Row className={'justify-content-center'}><Col className={'text-center'}><Spinner animation={'border'}/></Col></Row>;
+
 const Run: React.FC<runProps> = (props: runProps) => {
     const [loading, setLoading] = useState(true);
-    const [data, setData] = useState<runData>({id: 0, dateTime: '', deviceID: '', runID: 0, logId: 0});
+    const [data, setData] = useState<runWithJSONData>({id: 0, dateTime: '', deviceID: '', runID: 0, logId: 0, run:''});
+    const [prettyDate, setPrettyDate] = useState('');
 
     useEffect(() => {
         const load = async () => {
             //TOOD: fetch run data from /api/run/{props.id} or /api/run?id={props.id}
-            const response = await fetch(url+'/run?id='+props.id, {
+            const response = await fetch(url+'/run/get_run_json?id='+props.id, {
                 method: 'GET',
                 headers:{
                     'Content-Type': 'application/json'
@@ -27,7 +30,9 @@ const Run: React.FC<runProps> = (props: runProps) => {
             if(!response.ok) return console.log('Fetching run data failed...');
             const json = await response.json();
 
-            setData(json[0] as runData);
+            setData(json['Success'] as runWithJSONData);
+            setPrettyDate(new Date(json['Success']['dateTime']).toLocaleDateString('en-us'));
+
             setLoading(false);
         }
         load();
@@ -35,18 +40,33 @@ const Run: React.FC<runProps> = (props: runProps) => {
 
     return(
         <Container id={'runContainer'}>
-            <Row className={'justify-content-center'}>
-                <Col className={'text-center'}>
-                    {
-                        loading ? <Spinner animation={'border'}/> : <h3>{data.runID + data.deviceID + data.dateTime}</h3>
-                    }
-                </Col>
-            </Row>
-            <Row className={'justify-content-center mt-2'}>
-                <Col className={'text-center'}>
-                    <Button type={'button'} variant={'success'} onClick={() => {props.changeView('table')}}>Search Runs</Button>
-                </Col>
-            </Row>
+            {
+                loading ? loadingSpinner :
+                    <>
+                        <Row className={'justify-content-center'}>
+                            <Col className={'text-center mt-2'} xs={12} sm={6} md={4} lg={4} xl={4}>
+                                <InputGroup>
+                                    <InputGroup.Prepend><InputGroup.Text>Date</InputGroup.Text></InputGroup.Prepend>
+                                    <FormControl value={prettyDate} disabled={true}/>
+                                </InputGroup>
+                            </Col>
+                            <Col className={'text-center mt-2'} xs={12} sm={6} md={4} lg={4} xl={4}>
+                                <InputGroup>
+                                    <InputGroup.Prepend><InputGroup.Text>Device</InputGroup.Text></InputGroup.Prepend>
+                                    <FormControl value={data.deviceID} disabled={true}/>
+                                </InputGroup>
+                            </Col>
+                        </Row>
+                        <Row className={'justify-content-center mt-2'}>
+                            <Col className={'text-center'}>
+                                <InputGroup>
+                                    <InputGroup.Prepend><InputGroup.Text>Run Data</InputGroup.Text></InputGroup.Prepend>
+                                    <FormControl as={'textarea'} value={JSON.stringify(data.run, null, 2)} disabled={true}/>
+                                </InputGroup>
+                            </Col>
+                        </Row>
+                    </>
+            }
         </Container>
     )
 }
